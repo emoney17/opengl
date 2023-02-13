ffaa08e9fafb669ed7dc3d15a7400b8639877306
emoney17 <emmanuel.million@tutanota.com>
2023-02-13 14:02:36 -0600

    texture working

--- texture/texture.cpp
+++ texture/texture.cpp
@@ -76,10 +76,17 @@ int main(void)
 
   //============================================================TEXTURES
 
-  // Create an ID for the texture
-  unsigned int texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture);
+  // Create an ID for the textures
+  unsigned int texture1, texture2;
+  int width, height, nrChannels;
+  unsigned char* data;
+
+  // Tell stb_image.h to flip loaded textures on the y-axis
+  stbi_set_flip_vertically_on_load(true);
+
+  // Texture 1
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
 
   // Set texture wrapping/filtering options on the currently bound texture
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -88,8 +95,7 @@ int main(void)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
   // Load texture data from file
-  int width, height, nrChannels;
-  unsigned char* data = stbi_load("./container.jpg", &width, &height, &nrChannels, 0);
+  data = stbi_load("./background.jpg", &width, &height, &nrChannels, 0);
   if (data)
   {
     // Generate texture from the data as well as the mipmap
@@ -101,9 +107,39 @@ int main(void)
     fprintf(stderr, "Error loading texture\n");
     return -1;
   }
-
   // Afterwards free image data
   stbi_image_free(data);
+
+  // Texture 2
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+
+  // Set texture wrapping/filtering options on the currently bound texture
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // Load texture data from file
+  data = stbi_load("./konata.png", &width, &height, &nrChannels, 0);
+  if (data)
+  {
+    // Generate texture from the data as well as the mipmap
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+    fprintf(stderr, "Error loading texture\n");
+    return -1;
+  }
+  // Afterwards free image data
+  stbi_image_free(data);
+
+  // Activate shader before settings uniform for the two textures
+  glUseProgram(shader_program);
+  glUniform1i(glGetUniformLocation(shader_program, "texture1"), 0);
+  glUniform1i(glGetUniformLocation(shader_program, "texture2"), 1);
 
   //============================================================GAMELOOP
 
@@ -116,7 +152,11 @@ int main(void)
     glUseProgram(shader_program);
 
     // Bind the texture before drawing elements
-    glBindTexture(GL_TEXTURE_2D, texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
